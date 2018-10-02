6f62480b610cd8ef3ef7a08e6e0029618f5cf083
Lino <lino.virgen96@gmail.com>
2018-10-02 16:02:03 -0500

    slider update

--- scripts/updateTime.js
+++ scripts/updateTime.js
@@ -1,18 +1,21 @@
+function resetTime()
+{
+    
+}
+
+
 function updateTime()
 {
     time = parseInt(document.getElementById("slider").value);
+    document.getElementById("demo").innerHTML = time;
 
     for( var rack=1; rack<=RACK_NUM; rack++ )
-    {
         for( var host=1; host<=HOST_NUM; host++ )
-        {
             for( var cpu=1; cpu<=CPU_NUM; cpu++ )
-            {
                 if( hostObj[rack][host] )
+                {
                     updateTemperature( [rack,host,cpu,time], hostObj[rack][host][cpu] );
-            }
-        }
-    }
+                }
 }
 
 function updateTemperature( keys, obj )
