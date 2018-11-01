0001d7b8dfc32bad44bb8627a0fcbaf8c4960902
Lino <lino.virgen96@gmail.com>
2018-11-01 16:16:27 -0500

    interaction enabled

--- scripts/main.js
+++ scripts/main.js
@@ -110,7 +110,7 @@ function init()
     initRenderer();
 
     window.addEventListener( 'resize', onResize, false );
-    // window.addEventListener( 'mousedown', onMouseDown, false );
+    window.addEventListener( 'mousedown', onMouseDown, false );
     window.requestAnimationFrame( render );
 
     // request();
